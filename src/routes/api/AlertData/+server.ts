ee0a369f76113516578487911bce69cba1dd3f70
Ryan Adam <ryan.z.adam@gmail.com>
2023-05-09 11:24:31 +1200

    cleaned up

--- src/routes/api/AlertData/+server.ts
+++ src/routes/api/AlertData/+server.ts
@@ -1,9 +1,7 @@
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import prisma from "$lib/prisma";
 import { getPhenomenaData } from "./getPhenomena";
 
 export const GET: RequestHandler = async () => {
-
     return json(await getPhenomenaData())
 }
