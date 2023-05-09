cfa46f06fdf8a4298d4a172d970e408c74cbf676
TheRangiCrew <ryan.z.adam@gmail.com>
2023-05-10 00:21:29 +1200

    Renamed

--- src/lib/stores/AlertData.ts
+++
@@ -1,7 +0,0 @@
-import { writable, type Writable } from "svelte/store";
-import type { AlertDataDB } from "../../routes/api/AlertData/getPhenomena";
-
-
-export const alerts: Writable<AlertDataDB> = writable([])
-
-
---
+++ src/lib/stores/Alerts.ts
@@ -0,0 +1,25 @@
+import { writable, type Writable } from "svelte/store";
+import type { AlertDataDB } from "../../routes/api/AlertData/getPhenomena";
+
+export type AlertData = {
+    data: AlertDataDB,
+    count: number
+}
+
+export const alerts: Writable<AlertData> = writable({
+    data: [],
+    count: 0
+});
+
+export const setAlerts = (data: AlertDataDB) => {
+    let count = 0
+
+    data.forEach(e => {
+        count += e.Alerts.length;
+    })
+
+    alerts.set({
+        data,
+        count
+    })
+}
